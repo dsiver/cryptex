24419c7be68252bde73713a4878ce45988762b93
David Siver <dsiver1@my.westga.edu>
2015-10-05 23:22:34 -0400

    formatted code, renamed inputPosition inputCount, comparing userInput to password

--- cryptex.ino
+++ cryptex.ino
@@ -12,43 +12,57 @@
 #define DEBUG 1
 #define PASSWORD_LENGTH 8
 
-int password[PASSWORD_LENGTH] = 
-{SWITCH_UP, SWITCH_UP, SWITCH_DOWN, SWITCH_DOWN, 
-SWITCH_LEFT, SWITCH_RIGHT, SWITCH_LEFT, SWITCH_RIGHT};
+int password[PASSWORD_LENGTH] =
+{ SWITCH_UP, SWITCH_UP, SWITCH_DOWN, SWITCH_DOWN,
+  SWITCH_LEFT, SWITCH_RIGHT, SWITCH_LEFT, SWITCH_RIGHT
+};
 int userInput[PASSWORD_LENGTH];
-int inputPosition;
+int inputCount;
 
 void setup() {
   clearUserInput();
-  inputPosition = 0;
+  inputCount = 0;
 }
 
 void loop() {
   readButtons();
+  if (isPassword()) {
+
+  }
 }
 
-void readButtons(){
-  if(Esplora.readButton(SWITCH_UP) == LOW){
-     setUserInput(SWITCH_UP);
+boolean isPassword() {
+  if (inputCount == PASSWORD_LENGTH && isPassword()) {
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+      if (password[i] != userInput[i]) {
+        return false;
+      }
+    }
   }
-  else if (Esplora.readButton(SWITCH_DOWN) == LOW){
+}
+
+void readButtons() {
+  if (Esplora.readButton(SWITCH_UP) == LOW) {
+    setUserInput(SWITCH_UP);
+  }
+  else if (Esplora.readButton(SWITCH_DOWN) == LOW) {
     setUserInput(SWITCH_DOWN);
   }
-  else if (Esplora.readButton(SWITCH_LEFT) == LOW){
+  else if (Esplora.readButton(SWITCH_LEFT) == LOW) {
     setUserInput(SWITCH_LEFT);
   }
-  else if (Esplora.readButton(SWITCH_RIGHT) == LOW){
+  else if (Esplora.readButton(SWITCH_RIGHT) == LOW) {
     setUserInput(SWITCH_RIGHT);
   }
 }
 
-void setUserInput(int value){
-  userInput[inputPosition] = value;
-  inputPosition +=1;
+void setUserInput(int value) {
+  userInput[inputCount] = value;
+  inputCount += 1;
 }
 
-void clearUserInput(){
-  for(int i = 0; i < PASSWORD_LENGTH; i++){
+void clearUserInput() {
+  for (int i = 0; i < PASSWORD_LENGTH; i++) {
     userInput[i] = 0;
   }
 }
